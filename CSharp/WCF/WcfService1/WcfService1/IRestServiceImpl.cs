4e09d1f5759ae2c0b74a7f8512a36a62950b331a
Kinshuk <kinshuk.ram@gmail.com>
2015-04-19 23:50:52 +0530

    Adding the rest based wcf service

---
+++ CSharp/WCF/WcfService1/WcfService1/IRestServiceImpl.cs
@@ -0,0 +1,26 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+
+namespace RestService
+{
+    
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+       //call the service by URL - http://localhost:58907/RestServiceImpl.svc/xml/123
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Xml,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "xml/{id}")]
+        string XMLData(string id);
+
+        //call the service by URL - http://localhost:58907/RestServiceImpl.svc/json/123
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+            ResponseFormat = WebMessageFormat.Json,
+            BodyStyle = WebMessageBodyStyle.Wrapped,
+            UriTemplate = "json/{id}")]
+        string JSONData(string id);
+    }
+}
---
+++ CSharp/WCF/WcfService1/WcfService1/RestServiceImpl.svc.cs
@@ -0,0 +1,19 @@
+﻿namespace RestService
+{
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        #region IRestServiceImpl Members
+
+        public string XMLData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        public string JSONData(string id)
+        {
+            return "You requested product " + id;
+        }
+
+        #endregion
+    }
+}
